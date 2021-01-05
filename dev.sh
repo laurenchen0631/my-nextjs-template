9d2e9cd9b5486d10e1a0faf734fe9c35c9543187
Lauren <laurenchen0631@gmail.com>
2021-01-05 11:16:19 +0800

    support windows

--- dev.sh
+++ dev.sh
@@ -1,4 +1,9 @@
 ip=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n 1)
+if [ -z "$ip" ]
+then
+  ip=$(ipconfig | grep -E -i "IPv4 Address" | grep -E -o "[0-9][0-9.]+" | head -1)
+fi
+
 DATE=`date +%Y.%m.%d.%H.%M`
 
 function cleanup {
@@ -17,6 +22,6 @@ HTTPS_PORT=$(($PORT + 1000))
 
 trap cleanup EXIT;
 
-docker build -f Dockerfile.dev -t example-dev:$DATE .
+docker build --build-arg PORT=${PORT} -f Dockerfile.dev -t example-dev:$DATE .
 docker run --add-host="host:${ip}" -p ${HTTP_PORT}:80 -p ${HTTPS_PORT}:443 -d --name example-dev example-dev:$DATE
 yarn next dev -p ${PORT}
